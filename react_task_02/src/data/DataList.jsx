66d185b68c1fd0fa8a0229d922a7d13d10e6382a
excel <axceldesigns@gmail.com>
2025-06-22 15:34:44 +0100

    task02

--- react_task_02/src/data/DataList.jsx
+++ react_task_02/src/data/DataList.jsx
@@ -31,51 +31,51 @@ export const galleryData = [
   {
     id: 1,
     src: "/src/assets/picGallery/Rectangle22.png",
-    text: "Gallery image 1 description",
+    text: "Gallery image 1",
   },
   {
     id: 2,
     src: "/src/assets/picGallery/Rectangle23.png",
-    text: "Gallery image 2 description",
+    text: "Gallery image 2",
   },
   {
     id: 3,
     src: "/src/assets/picGallery/Rectangle24.png",
-    text: "Gallery image 3 description",
+    text: "Gallery image 3 ",
   },
   {
     id: 4,
     src: "/src/assets/picGallery/Rectangle25.png",
-    text: "Gallery image 4 description",
+    text: "Gallery image 4 ",
   },
   {
     id: 5,
     src: "/src/assets/picGallery/Rectangle26.png",
-    text: "Gallery image 5 description",
+    text: "Gallery image 5 ",
   },
   {
     id: 6,
     src: "/src/assets/picGallery/Rectangle27.png",
-    text: "Gallery image 6 description",
+    text: "Gallery image 6 ",
   },
   {
     id: 7,
     src: "/src/assets/picGallery/Rectangle28.png",
-    text: "Gallery image 7 description",
+    text: "Gallery image 7 ",
   },
   {
     id: 8,
     src: "/src/assets/picGallery/Rectangle29.png",
-    text: "Gallery image 8 description",
+    text: "Gallery image 8 ",
   },
   {
     id: 9,
     src: "/src/assets/picGallery/Rectangle30.png",
-    text: "Gallery image 9 description",
+    text: "Gallery image 9 ",
   },
   {
     id: 10,
     src: "/src/assets/picGallery/Rectangle31.png",
-    text: "Gallery image 10 description",
+    text: "Gallery image 10 ",
   },
 ];
