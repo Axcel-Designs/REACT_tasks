f16d0fcf5e5c67c149300a13b9a3912de405236f
excel <axceldesigns@gmail.com>
2025-06-22 22:41:26 +0100

    task02

--- react_task_02/src/components/footer/FootContacts.jsx
+++ react_task_02/src/components/footer/FootContacts.jsx
@@ -4,17 +4,17 @@ export default function FootContacts() {
   return (
     <section className="grid justify-center align-center">
       <div className="flex flex-col space-y-4">
-        <h3 className="font-semibold">Contacts</h3>
-        <ul className="flex flex-col space-y-2">
-          <li>
+        <h3 className="font-semibold text-center md:text-left">Contacts</h3>
+        <ul className="flex flex-col space-y-2 justify-center align-center ">
+          <li className="text-center md:text-left">
             <i class="fa-solid fa-location-dot"></i>
             <span className="ml-4">123 Main St, Anytown, USA</span>
           </li>
-          <li>
-            <i class="fa-solid fa-phone"></i>
+          <li className="text-center md:text-left">
+            <i class="fa-solid fa-phone "></i>
             <span className="ml-4">+234 123 4567</span>
           </li>
-          <li>
+          <li className="text-center md:text-left">
             <i class="fa-solid fa-envelope"></i>
             <Link to="mailto:info@example.com">
               <span className="ml-4">info@example.com</span>
--- react_task_02/src/components/footer/FootInformation.jsx
+++ react_task_02/src/components/footer/FootInformation.jsx
@@ -5,11 +5,11 @@ export default function FootInformation() {
   return (
     <section className="grid justify-center align-center">
       <div className="flex flex-col space-y-4">
-        <h3 className="font-semibold">Information</h3>
+        <h3 className="font-semibold text-center md:text-left">Information</h3>
         <div>
           <ul className="flex flex-col space-y-2">
             {navbarLinks.map((link) => (
-              <li key={link.to}>
+              <li key={link.to} className="text-center md:text-left">
                 <Link to={link.to}>{link.label}</Link>
               </li>
             ))}
