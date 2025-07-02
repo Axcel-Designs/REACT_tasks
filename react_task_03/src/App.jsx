b4490c02367327df0c499d36d13e8dcc3fdc3de9
excel <axceldesigns@gmail.com>
2025-07-02 02:14:16 +0100

    task

--- react_task_03/src/App.jsx
+++ react_task_03/src/App.jsx
@@ -1,12 +1,4 @@
 // import { useState } from "react";
-import Header from "./components/Header";
-import Test, { NamesOfPeople,Form, List } from "./components/rendering_list/Test";
-import WelcomeCard from "./components/WelcomeCard";
-import {PracticeCard,Gallery} from "./components/rendering_list/Test";
-import Fun from "./components/Fun";
-import Home from "./pages/Home";
-import ContolForm from "./pages/ControlForm";
-import FormikForm from "./components/forms/FormikForm";
 
 
 function App() {
