a7ca398a0edb2b556e10edd27becf22681fbb644
excel <axceldesigns@gmail.com>
2025-07-14 19:21:48 +0100

    task

--- react_task_03/src/pages/SuccessRegister.jsx
+++ react_task_03/src/pages/SuccessRegister.jsx
@@ -9,19 +9,23 @@ export default function SuccessRegister() {
 
   function handleLogin() {
     setActiveView("login");
-    navigate('/')
+    navigate("/");
   }
   return (
     <>
-      <section className="rounded-lg ">
-        <img src={img} alt="" className="bg-red-50 w-full" />
-        <div className="p-4">
-          <h1 className="flex flex-wrap text-4xl font-extrabold text-gray-900 my-4">
-            You Have Successully Registered!
-          </h1>
-          <Button label={"Go to Login"} link={handleLogin} />
-        </div>
-      </section>
+      <main className=" w-full h-svh flex justify-around items-center">
+        <section className="rounded-2xl w-3/5 min-h-[400px] lg:w-2/5  bg-white text-gray-700 shadow-lg/30">
+          <div>
+            <img src={img} alt="" className="bg-red-50 w-full rounded-t-2xl" />
+          </div>
+          <div className="p-4 ">
+            <h1 className="flex flex-wrap text-2xl lg:text-3xl font-extrabold text-gray-900 my-4">
+              You Have Successully Registered!
+            </h1>
+            <Button label={"Go to Login"} link={handleLogin} />
+          </div>
+        </section>
+      </main>
     </>
   );
 }
