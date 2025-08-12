0e1411ee90e91a0ceac5208d8ffdce60bb7ad778
excel <axceldesigns@gmail.com>
2025-08-12 19:42:44 +0100

    task

--- react_task_06/app/layout.js
+++ react_task_06/app/layout.js
@@ -19,6 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/axcelimg.png" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
