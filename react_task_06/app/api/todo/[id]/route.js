159e2b34f54b31585aa9b7032354327a62f5b077
excel <axceldesigns@gmail.com>
2025-08-13 19:13:06 +0100

    task

--- react_task_06/app/api/todo/[id]/route.js
+++ react_task_06/app/api/todo/[id]/route.js
@@ -1,9 +1,41 @@
-export async function GET(req) {}
+import todoData from "@/lib/data";
+import { NextResponse } from "next/server";
 
-export async function PUT(req) {}
+export async function GET(req, { params }) {
+  const { id } = params;
+  const todo = todoData.find((item) => item.id === parseInt(id));
+  if (!todo) {
+    return NextResponse.json({ message: "Todo not found" }, { status: 404 });
+  }
+  return NextResponse.json(todo);
+}
 
-export async function DELETE(req) {}
+export async function PUT(req, { params }) {
+  const  id  = await params.id;
+  const { courseCode, course, Dept } = await req.json();
 
-export async function PATCH(req) {}
+  const index = todoData.findIndex((todo) => todo.id === parseInt(id));
+  if (index === -1) {
+    return NextResponse.json({ message: "Todo not found" }, { status: 404 });
+  }
 
+  todoData[index] = {
+    ...todoData[index],
+    courseCode,
+    course,
+    Dept,
+  };
 
+  return NextResponse.json(todoData[index], { status: 200 });
+}
+
+export async function DELETE(req, { params }) {
+  const { id } = params;
+  const index = todoData.findIndex((item) => item.id === parseInt(id));
+  if (index === -1) {
+    return NextResponse.json({ message: "unavailable" }, { status: 404 });
+  }
+
+  const deletedItem = todoData.splice(index, 1);
+  return NextResponse.json(deletedItem[0], { status: 200 });
+}
--- react_task_06/app/api/todo/route.js
+++ react_task_06/app/api/todo/route.js
@@ -1,10 +1,23 @@
+import todoData from "@/lib/data";
 import { NextResponse } from "next/server";
 
-export async function GET(req) {
-  return NextResponse.json()
+export async function GET() {
+  return NextResponse.json(todoData);
 }
 
-export async function POST(req) {}
-
-
-
+export async function POST(req) {
+  try {
+    const { courseCode, course, Dept } = await req.json();
+    const newTodo = {
+      id: todoData.length === 0 ? 1 : todoData[todoData.length - 1].id + 1,
+      courseCode: courseCode,
+      course: course,
+      Dept: Dept,
+    };
+    todoData.push(newTodo);
+    return NextResponse.json(newTodo, { status: 201 });
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({ message: "Invalid" }, { status: 400 });
+  }
+}
